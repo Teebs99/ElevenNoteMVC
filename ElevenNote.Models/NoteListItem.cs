d300a0efa3c4172192bbbb768758ce5252d91487
Stephen Ives <teebs99@gmail.com>
2021-08-11 11:00:23 -0400

    ListItem & Create

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+   public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset CreateUtc { get; set; }
+    }
+}
